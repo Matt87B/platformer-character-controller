5dd1211f1082e038f5dc8fd54c7f03f865df9df6
Matt87B <Matthew87Bowman@gmail.com>
2026-02-11 18:33:42 -0500

    modify water poly

--- scripts/water/WaterBody.gd
+++ scripts/water/WaterBody.gd
@@ -14,8 +14,10 @@ var water_width := 0
 @export var impulse_factor := 0.05
 
 @onready var area := $Area2D
-@onready var polygon : CollisionPolygon2D = $Area2D/CollisionPolygon2D
+@onready var collision_poly : CollisionPolygon2D = $Area2D/CollisionPolygon2D
 @onready var bottom_path := $Line2D
+
+var water_poly : Polygon2D = Polygon2D.new()
 
 func _ready() -> void:
 	for j in range(tile_count * 8):
@@ -25,31 +27,25 @@ func _ready() -> void:
 		p.velocity = 0.0
 		points.append(p)
 	
-	water_width = (points.size() - 1)
-	
-	build_collision_from_path()
-
-func build_collision_from_path():
-	var poly = PackedVector2Array()
-	poly.append(Vector2(0, base_height))
-	poly.append(Vector2(water_width + 1, base_height))
-	poly.append(Vector2(water_width + 1, 5 + base_height))
-	poly.append(Vector2(0, 5 + base_height))
-
-	polygon.polygon = poly
+	water_width = points.size()
+	collision_poly.polygon = PackedVector2Array ([
+		Vector2(0, base_height),
+		Vector2(water_width, base_height),
+		Vector2(water_width, base_height + 10),
+		Vector2(0, base_height + 10)
+		])
 
 func _draw():
-	var poly : Polygon2D = Polygon2D.new()
 	var heights := []
 
-	poly.polygon = bottom_path.points
+	water_poly.polygon = bottom_path.points
 
 	for i in range(points.size()):
 		var y = round(points[i].height)
 		heights.append(y)
-		#poly.append(Vector2(i, y))
+		water_poly.polygon.append(Vector2(i, y))
 
-	draw_polygon(poly.polygon, [Color(0.2, 0.5, 1.0, 0.6)])
+	draw_polygon(water_poly.polygon, [Color(0.2, 0.5, 1.0, 0.6)])
 
 	for i in range(heights.size() - 1):
 		draw_pixel_line(i, heights[i], i + 1, heights[i + 1])
