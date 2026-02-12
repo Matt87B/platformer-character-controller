ca4f8cb32ca17151e53a1d3ca9db64950da32514
Matt87B <matthew87bowman@gmail.com>
2026-02-11 23:59:13 -0500

    further polish water

--- scripts/water/WaterBody.gd
+++ scripts/water/WaterBody.gd
@@ -1,23 +1,21 @@
 extends Node2D
 
 var points: Array[WaterPoint] = []
-var bottom_points: Array = []
+var bodies_in_water := []
 var water_width := 0
 
 @export var tile_count := 13
-@export var base_height := 0
-@export var tension := 0.2
+@export var base_height := 1
+@export var tension := 0.1
 @export var non_linear_dampening := 0.015
-@export var dampening := 0.95
+@export var dampening := 0.97
 @export var spread := 0.5
 @export var iterations := 16
-@export var impulse_factor := 0.05
+@export var impulse_factor := 0.04
 
-@onready var area := $Area2D
+
 @onready var collision_poly : CollisionPolygon2D = $Area2D/CollisionPolygon2D
 @onready var bottom_path := $Line2D
-
-var water_poly : Polygon2D = Polygon2D.new()
 
 func _ready() -> void:
 	for j in range(tile_count * 8):
@@ -28,53 +26,58 @@ func _ready() -> void:
 		points.append(p)
 	
 	water_width = points.size()
-<<<<<<< HEAD
-	path_length = bottom_path.curve.get_baked_length()
 	
-	build_collision_from_path()
-
-func build_collision_from_path():
-	var poly = PackedVector2Array()
-	poly.append(Vector2(0, base_height))
-	poly.append(Vector2(points.size(), base_height))
-	poly.append(Vector2(points.size(), 5 + base_height))
-	poly.append(Vector2(0, 5 + base_height))
-
-	polygon.polygon = poly
-=======
+	#Initialize the collision polygon
 	collision_poly.polygon = PackedVector2Array ([
 		Vector2(0, base_height),
 		Vector2(water_width, base_height),
-		Vector2(water_width, base_height + 10),
-		Vector2(0, base_height + 10)
+		Vector2(water_width, base_height + 8),
+		Vector2(0, base_height + 8)
 		])
->>>>>>> 5dd1211f1082e038f5dc8fd54c7f03f865df9df6
 
+#Draws the water's surface and body
 func _draw():
 	var heights := []
-
-	water_poly.polygon = bottom_path.points
-
+	var bottom_points = bottom_path.points
+	var water_poly := PackedVector2Array()
+	
+	#Append Line2D points to the polygon from right to left
+	for i in range(bottom_points.size() - 1, -1, -1):
+		water_poly.append(bottom_points[i])
+	
+	#Append water surface points to the polygon from left to right
 	for i in range(points.size()):
-		var y = round(points[i].height)
-		heights.append(y)
-<<<<<<< HEAD
-		poly.append(Vector2(i, y))
+		#Store heights value into heights array for future reference
+		heights.append(round(points[i].height))
+		var y = heights[i]
+		water_poly.append(Vector2(i, y))
+		
 	
-	for i in range(curve.get_point_count() - 1, -1, -1):
-		var p = to_local(bottom_path.to_global(curve.get_point_position(i)))
-		poly.append(p)
-	draw_polygon(poly, [Color(0.2, 0.5, 1.0, 0.6)])
+	#Add last point at the end of the water surface line to complete the polygon
+	water_poly.append(Vector2(water_width, base_height))
 	
-=======
-		water_poly.polygon.append(Vector2(i, y))
-
-	draw_polygon(water_poly.polygon, [Color(0.2, 0.5, 1.0, 0.6)])
-
->>>>>>> 5dd1211f1082e038f5dc8fd54c7f03f865df9df6
+	#Draw water fill polygon
+	draw_polygon(water_poly, [Color(0.2, 0.5, 1.0, 0.6)])
+	
+	#Draw the water's surface
 	for i in range(heights.size() - 1):
 		draw_pixel_line(i, heights[i], i + 1, heights[i + 1])
 
+func get_bottom_y_at_x(x: float) -> float:
+	var pts = bottom_path.points
+
+	for i in range(pts.size() - 1):
+		var p1 = pts[i]
+		var p2 = pts[i + 1]
+		if x >= p1.x and x <= p2.x:
+			var t = (x - p1.x) / (p2.x - p1.x)
+			return lerp(p1.y, p2.y, t)
+	#If x is outside the line
+	if x < pts[0].x:
+		return pts[0].y
+	return pts[-1].y
+
+#Draw the water surface line as pixels
 func draw_pixel_line(x1, y1, x2, y2):
 	var dx = x2 - x1
 	var dy = y2 - y1
@@ -86,17 +89,17 @@ func draw_pixel_line(x1, y1, x2, y2):
 		draw_rect(Rect2(px, py, 1, 1), Color(0.2, 0.5, 1.0))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	for p in points:
 		var force = tension * (p.rest_height - p.height)
 		p.velocity += force
 		p.velocity *= dampening
 		p.velocity *= 1.0 - abs(p.velocity) * non_linear_dampening
 		p.height += p.velocity
-		#var bottom_y = p.rest_height + 8
-		#if p.height > bottom_y:
-			#p.height = bottom_y
-			#p.velocity = 0
+		#Prevent overshoot down the way
+		var bottom_y = get_bottom_y_at_x(points.find(p))
+		if p.height > bottom_y:
+			p.height = bottom_y
 	
 	var iter_spread = spread/iterations
 	for i in range(iterations):
@@ -108,6 +111,7 @@ func _process(_delta: float) -> void:
 		points[0].velocity *= 0.5
 		points[points.size() - 1].velocity *= 0.5
 	
+	apply_body_forces()
 	queue_redraw()
 
 class WaterPoint:
@@ -121,9 +125,12 @@ func splash(index: int, impulse: float):
 	if index > 0: points[index - 1].velocity += impulse * impulse_factor * 0.5
 	if index < points.size() - 1: points[index + 1].velocity += impulse * impulse_factor * 0.5
 
+func apply_body_forces():
+	for body in bodies_in_water:
+		pass
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	var local_x = body.global_position.x - global_position.x
-	var index = int(local_x)
-	index = clamp(index, 0, points.size() - 1)
-	
-	splash(index, body.velocity.y)
+	bodies_in_water.append(body)
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	bodies_in_water.erase(body)
