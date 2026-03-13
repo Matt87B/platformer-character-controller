3c15aeede1e9c8cd4cace587316e665c5fbb6a9d
Matt87B <matthew87bowman@gmail.com>
2026-03-13 00:42:11 -0400

    feat: design options menu.

--- scripts/user_interface/main_menu/MainMenu.gd
+++ scripts/user_interface/main_menu/MainMenu.gd
@@ -1,16 +1,5 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/node_2d.tscn")
 
@@ -20,7 +9,7 @@ func _on_load_pressed() -> void:
 
 
 func _on_options_pressed() -> void:
-	print("Pressed options.")
+	get_tree().change_scene_to_file("res://scenes/options.tscn")
 
 
 func _on_quit_pressed() -> void:
